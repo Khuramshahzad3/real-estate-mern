781ac525752ca3d62e7affbbdb5909756517c497
Khuram Shahzad <Khuramshahzad54676@gmail.com>
2024-09-09 16:58:47 +0500

    Pre Installation

--- api/index.js
+++ api/index.js
@@ -5,8 +5,11 @@ import UserRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 import ListingRouter from './routes/listing.routes.js'
+import path from 'path';
+import exp from "constants";
 dotenv.config();
 const PORT = 3000;
+const __dirname=path.resolve();
 const app = express();
 mongoose
   .connect(process.env.MONGO)
@@ -23,7 +26,10 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/user", UserRouter);
 app.use("/api/listing", ListingRouter);
-
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  })
 app.use((err, req, res, next) => {
   const StatusCode = err.StatusCode || 500;
   const message = err.message || "Internal Server Error";
